12bbea1d016b48f26216d51511add898c5c63507
Rabiah Hogans <hogansrabiah@gmail.com>
2020-03-29 03:23:32 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,6 +4,7 @@ return "Hello, my name is #{name}."
 end
 
 def batch_badge_creator (names)
-  names = ("yas","bri", "netty", "dana") 
+  names = ["yas","bri", "netty", "dana"]
+  
   return "Hello, my name is #{name}."
 end
